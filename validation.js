5ef5be9379c51906b92d4d8d44cc7c13ab7a45d9
WALLESAMEHERE <hulk13@op.pl>
2016-09-29 20:53:23 +0200

    super valid

---
+++ validation.js
@@ -0,0 +1,138 @@
+$(document).ready(function() {
+    // Start validate form
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        var fields = $(this).find('input, textarea, select'); // get all controls
+        var wynik = 0; // global input result -  if input return true wynik++
+        for (var i = 0; i < fields.length; i++) {
+            var field = fields[i]; // form field
+            var dataAttr = $(field).data('walid'); // attribute name field
+            var attrName = dataAttr.split(' '); // separate validation rules
+            var fieldValue = $(field).val(); // entered value
+            var inputTab = []; // array with true false from  field
+            var inputscore = 0; // attribute result - if attribute return true inputscore++
+            for (j = 0; j < attrName.length; j++) {
+                var validRule = attrName[j]; // get validation rule
+                var ruleName = validRule.match(/[a-zA-Z]+/g); // only the name of the validation rule 
+                start(fieldValue, ruleName, validRule);
+                inputTab.push(start(fieldValue, ruleName, validRule, field)); // push true / false to array of validation rules 
+                // condition if rule is true +1 to attribute result
+                if (inputTab[j] === true) {
+                    inputscore++; 
+                }
+            }
+            // condition - if all rules from inuput == true from attribute rule - +1 to global result of this field
+            if (attrName.length == inputscore) {
+                wynik++;
+                $(field).removeClass('invalid'); // remove error class                 
+                $(field).addClass('valid'); // add success class when input return true
+                $(field).next('.inputErrorMsg').html(''); // remove message from error box
+            } else {
+                $(field).addClass('invalid'); // add error class when input return false
+                $(field).removeClass('valid'); // remove success class
+            }
+        }
+        // condition - if global result with true == all form inputs - send form
+        if (wynik == fields.length) {
+            this.submit();
+        }
+    });
+    // function with validation rules
+    function start(input, atrr, data, glob) {
+        var result = true;
+        var regula = {
+            required: function() {
+                if (input == "") {
+                    $('.blad').html('Wypełnij formularz');
+                    return false;
+                } else {
+                    $('.blad').html('');
+                    return true;
+                };
+            },
+            text: function() {
+                let reg = /^[a-zA-Z]+$/;
+                if (!input.match(reg)) {
+                    $(glob).next('.inputErrorMsg').html('wprowadz tylko tekst');
+                    return false;
+                } else {
+                    $(glob).next('.inputErrorMsg').html('');
+                    return true;
+                };
+            },
+            number: function() {
+                let stringTonumber = parseInt(input);
+                if (isNaN(stringTonumber)) {
+                    $(glob).next('.inputErrorMsg').html('wprowadz numer');
+                    return false;
+                } else {
+                    return true;
+                };
+            },
+            dataIsValid: function() {
+                var reg = /^\d{4}-\d{1,2}-\d{1,2}$/;
+                if (!input.match(reg)) {
+                    $(glob).next('.inputErrorMsg').html('zly format daty yyyy-mm-dd');
+                    return false;
+                } else {     
+                    return true;
+                };
+            },
+            urBoot: function() {
+                var gsDayNames = new Array(
+                    'niedziela',
+                    'poniedzialek',
+                    'wtorek',
+                    'sroda',
+                    'czwartek',
+                    'piatek',
+                    'sobota'
+                );
+                var d = new Date();
+                var dayName = gsDayNames[d.getDay()];
+                if (input == dayName) {
+                    $(glob).next('.inputErrorMsg').html('');
+                    return true;
+                } else {
+                    $(glob).next('.inputErrorMsg').html('wybierz dzisiejszy dzień');
+                    return false;
+                }
+            },
+            min: function() {
+                let numb = data.replace(/[^0-9]/g, '');
+                if (input.length < numb) {
+                    $(glob).next('.inputErrorMsg').html('wprowadz min 3 znaki');
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            max: function() {
+                let numb = data.replace(/[^0-9]/g, '');
+                if (input.length > numb) {
+                    $(glob).next('.inputErrorMsg').html('wprowadz max 15 znaków');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        process(regula[atrr], regula);
+        // condition - return true or false from valid rules
+        if (process(regula[atrr], regula) == false) {
+                var result = false;
+        } 
+        return result;
+    }
+    function process(callback, obj) {
+        var result = true;
+        if (callback('return') == true) {
+            callback.call(obj, null);
+            return true;
+        } else {
+            result = false;
+            return result;
+        }
+        return result;
+    }
+});
